c206e11f9fda8ae38794ba63725485dbc53f2d37
Chen, James SJ <82010157@corphq.hk.pccw.com>
2024-10-17 11:51:05 +0800

    fix: 修改内容

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -758,6 +758,7 @@ const $translate = Instance?.appContext.config.globalProperties.$translate;
       <p>Double: {{ double }}</p>
       <button @click="increment">Increment</button>
     </div> -->
+    <h1>哈哈哈哈哈，我在修改东西</h1>
     <button @click="updateThemeColor">Update Theme Color</button>
 
     <button @click="postTest1">测试post接口1</button>
