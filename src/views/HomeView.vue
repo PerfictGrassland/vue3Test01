856464d728e116d4a6f9cb73a4b2af062db904c2
Chen, James SJ <82010157@corphq.hk.pccw.com>
2024-10-17 15:25:27 +0800

    fix：更新yml

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -761,6 +761,9 @@ const $translate = Instance?.appContext.config.globalProperties.$translate;
     <h1>哈哈哈哈哈，我在修改东西11</h1>
     <h1>哈哈哈哈哈，我在修改东西22</h1>
     <h1>哈哈哈哈哈，我在修改东西33</h1>
+    <h1>哈哈哈哈哈，我在修改东西11</h1>
+    <h1>哈哈哈哈哈，我在修改东西22</h1>
+    <h1>哈哈哈哈哈，我在修改东西33</h1>
     <button @click="updateThemeColor">Update Theme Color</button>
 
     <button @click="postTest1">测试post接口1</button>
